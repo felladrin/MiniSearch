6b157f0b0c2b20896f60efb886c3a0b597fb85e1
Victor Nogueira <felladrin@gmail.com>
2023-10-21 16:04:27 +0300

    Reformat vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePluginNode } from "vite-plugin-node";
 
 export default defineConfig(({ command }) => ({
   build: {
-    target: 'esnext'
+    target: "esnext",
   },
   plugins: [
     react(),
