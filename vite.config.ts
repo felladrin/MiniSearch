1913a36b59c9a4a0f4cbceeb96fedfa73d869361
Victor Nogueira <felladrin@gmail.com>
2024-03-16 23:40:18 +0200

    Add `/wllama/esm` as an external dependency in Rollup Options

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ command }) => ({
   },
   build: {
     target: "esnext",
+    rollupOptions: {
+      external: ["/wllama/esm"],
+    },
   },
   worker: {
     format: "es",
