f16b14c2f9f498518c747db660b578ff5b433ccd
Victor Nogueira <felladrin@gmail.com>
2024-03-23 00:58:57 +0200

    Remove devTools module, which was preventing the app to work on iOS browser
    It seems `import.meta.env.DEV` was not working anymore and devTools was trying to load, causing lack of memory to load transformers.js.

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { prepareTextGeneration } from "./modules/textGeneration";
 import "water.css/out/water.css";
 import "react-tooltip/dist/react-tooltip.css";
 
-if (import.meta.env.DEV) import("./modules/devTools");
-
 prepareTextGeneration();
 
 createRoot(document.body.appendChild(document.createElement("div"))).render(
--- src/modules/devTools.ts
+++
@@ -1,9 +0,0 @@
-import "console-panel/src/console-panel.js";
-import "console-panel/src/console-panel.css";
-
-declare const consolePanel: {
-  enable: () => void;
-  disable: () => void;
-};
-
-consolePanel.enable();
