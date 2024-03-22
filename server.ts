67936f13d3dd858e8414fa833b00fc67e69145af
Victor Nogueira <felladrin@gmail.com>
2024-03-23 01:46:10 +0200

    Refactor search function to remove duplicate URLs

--- server.ts
+++ server.ts
@@ -4,21 +4,28 @@ import express from "express";
 async function search(query: string, limit?: number) {
   try {
     const url = new URL("http://127.0.0.1:8080/search");
+
     url.search = new URLSearchParams({
       q: query,
       language: "auto",
       safesearch: "0",
       format: "json",
     }).toString();
+
     const response = await fetch(url);
+
     let { results } = (await response.json()) as {
       results: { url: string; title: string; content: string }[];
     };
+
     const searchResults: [title: string, content: string, url: string][] = [];
+
     if (results) {
       if (limit && limit > 0) {
         results = results.slice(0, limit);
       }
+
+      const uniqueUrls = new Set<string>();
 
       for (const result of results) {
         const stripHtmlTags = (str: string) => str.replace(/<[^>]*>?/gm, "");
@@ -28,11 +35,17 @@ async function search(query: string, limit?: number) {
         if (content === "") continue;
 
         const title = stripHtmlTags(result.title);
+
         const url = result.url as string;
 
-        searchResults.push([title, content, url]);
+        if (!uniqueUrls.has(url)) {
+          uniqueUrls.add(url);
+
+          searchResults.push([title, content, url]);
+        }
       }
     }
+
     return searchResults;
   } catch (e) {
     console.error(e);
