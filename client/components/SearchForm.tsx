6a7fd21540807f7b66c17f70038d3b31dfc1c247
Victor Nogueira <felladrin@gmail.com>
2024-03-23 21:27:58 +0200

    Add suggested query functionality to SearchForm component

--- client/components/SearchForm.tsx
+++ client/components/SearchForm.tsx
@@ -1,20 +1,42 @@
-import { useEffect, useRef, FormEvent } from "react";
+import { useEffect, useRef, FormEvent, useState, useCallback } from "react";
 import { searchQueryKey } from "../modules/search";
+import { getRandomQuerySuggestion } from "../modules/querySuggestions";
 
 export function SearchForm() {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const searchQuery = localStorage.getItem(searchQueryKey) ?? "";
+  const [suggestedQuery, setSuggestedQuery] = useState<string>(() => {
+    const previousSearchQuery = localStorage.getItem(searchQueryKey) ?? "";
 
-  const startSearching = () => {
-    if (textAreaRef.current && textAreaRef.current.value.trim().length > 0) {
-      const encodedQuery = encodeURIComponent(textAreaRef.current?.value);
-      window.location.href = `./?q=${encodedQuery}`;
+    localStorage.removeItem(searchQueryKey);
+
+    return previousSearchQuery === ""
+      ? getRandomQuerySuggestion()
+      : previousSearchQuery;
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const userQueryIsBlank = event.target.value.trim().length === 0;
+    const suggestedQueryIsBlank = suggestedQuery.trim().length === 0;
+
+    if (userQueryIsBlank && suggestedQueryIsBlank) {
+      setSuggestedQuery(getRandomQuerySuggestion());
+    } else if (!userQueryIsBlank && !suggestedQueryIsBlank) {
+      setSuggestedQuery("");
     }
   };
 
+  const startSearching = useCallback(() => {
+    let queryToEncode = suggestedQuery;
+
+    if (textAreaRef.current && textAreaRef.current.value.trim().length > 0) {
+      queryToEncode = textAreaRef.current.value;
+    }
+
+    window.location.href = `./?q=${encodeURIComponent(queryToEncode)}`;
+  }, [suggestedQuery]);
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    localStorage.removeItem(searchQueryKey);
     startSearching();
   };
 
@@ -30,14 +52,14 @@ export function SearchForm() {
     return () => {
       textArea?.removeEventListener("keypress", keyboardEventHandler);
     };
-  }, [textAreaRef]);
+  }, [startSearching]);
 
   return (
     <form onSubmit={handleSubmit} style={{ width: "100%" }}>
       <textarea
-        placeholder="Anything you need!"
+        placeholder={suggestedQuery}
         ref={textAreaRef}
-        defaultValue={searchQuery}
+        onChange={handleInputChange}
         autoFocus
       />
       <button type="submit" style={{ width: "100%", marginTop: "20px" }}>
