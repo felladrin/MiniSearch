c13c1a9cc3cf3a6ba75ffa8e7fa30b0cd5a83524
Victor Nogueira <felladrin@gmail.com>
2024-04-17 23:56:35 +0100

    Use Vite Preview Server in place of Express

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,148 @@
 import { PreviewServer, ViteDevServer, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePluginNode } from "vite-plugin-node";
-import { crossOriginIsolationHeaders } from "./server/headers";
+import basicSSL from "@vitejs/plugin-basic-ssl";
+import fetch from "node-fetch";
 
-export default defineConfig(({ command }) => {
-  const crossOriginServerHook = <T extends ViteDevServer | PreviewServer>(
-    server: T,
-  ) => {
-    server.middlewares.use((_, response, next) => {
-      crossOriginIsolationHeaders.forEach(({ key, value }) => {
-        response.setHeader(key, value);
-      });
-      next();
+export default defineConfig(() => ({
+  root: "./client",
+  server: {
+    host: process.env.HOST,
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+    hmr: {
+      port: process.env.HMR_PORT ? Number(process.env.HMR_PORT) : undefined,
+    },
+  },
+  preview: {
+    host: process.env.HOST,
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+  },
+  build: {
+    target: "esnext",
+  },
+  plugins: [
+    process.env.BASIC_SSL === "true" ? basicSSL() : undefined,
+    react(),
+    {
+      name: "configure-server-cross-origin-isolation",
+      configureServer: crossOriginServerHook,
+      configurePreviewServer: crossOriginServerHook,
+    },
+    {
+      name: "configure-server-search-endpoint",
+      configureServer: searchEndpointServerHook,
+      configurePreviewServer: searchEndpointServerHook,
+    },
+  ],
+}));
+
+function crossOriginServerHook<T extends ViteDevServer | PreviewServer>(
+  server: T,
+) {
+  server.middlewares.use((_, response, next) => {
+    crossOriginIsolationHeaders.forEach(({ key, value }) => {
+      response.setHeader(key, value);
     });
-  };
+    next();
+  });
+}
 
-  return {
-    root: "./client",
-    server: {
-      port: process.env.PORT ? Number(process.env.PORT) : 7860,
-      hmr: {
-        port: process.env.HMR_PORT ? Number(process.env.HMR_PORT) : 7861,
-      },
-    },
-    build: {
-      target: "esnext",
-    },
-    plugins: [
-      react(),
-      {
-        name: "configure-server-cross-origin-isolation",
-        configureServer: crossOriginServerHook,
-        configurePreviewServer: crossOriginServerHook,
-      },
-      ...(command === "serve"
-        ? VitePluginNode({
-            adapter: ({ app, req, res, next }) => {
-              if (req.url.startsWith("/search")) {
-                app(req, res);
-              } else {
-                next();
-              }
-            },
-            appPath: "./server/index.ts",
-            exportName: "app",
-          })
-        : []),
-    ],
-  };
-});
+function searchEndpointServerHook<T extends ViteDevServer | PreviewServer>(
+  server: T,
+) {
+  server.middlewares.use(async (request, response, next) => {
+    if (!request.url.startsWith("/search")) {
+      next();
+      return;
+    }
+
+    const { searchParams } = new URL(
+      request.url,
+      `http://${request.headers.host}`,
+    );
+
+    const query = searchParams.get("q");
+
+    if (!query) {
+      response.statusCode = 400;
+      response.end("Missing the query parameter.");
+      return;
+    }
+
+    const limitParam = searchParams.get("limit");
+
+    const limit =
+      limitParam && Number(limitParam) > 0 ? Number(limitParam) : undefined;
+
+    const searchResults = await fetchSearXNG(query, limit);
+
+    response.end(JSON.stringify(searchResults));
+  });
+}
+
+/** Server headers for cross origin isolation, which enable clients to use `SharedArrayBuffer` on the Browser. */
+export const crossOriginIsolationHeaders: { key: string; value: string }[] = [
+  {
+    key: "Cross-Origin-Embedder-Policy",
+    value: "require-corp",
+  },
+  {
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Cross-Origin-Resource-Policy",
+    value: "cross-origin",
+  },
+];
+
+export async function fetchSearXNG(query: string, limit?: number) {
+  try {
+    const url = new URL("http://127.0.0.1:8080/search");
+
+    url.search = new URLSearchParams({
+      q: query,
+      language: "auto",
+      safesearch: "0",
+      format: "json",
+    }).toString();
+
+    const response = await fetch(url);
+
+    let { results } = (await response.json()) as {
+      results: { url: string; title: string; content: string }[];
+    };
+
+    const searchResults: [title: string, content: string, url: string][] = [];
+
+    if (results) {
+      if (limit && limit > 0) {
+        results = results.slice(0, limit);
+      }
+
+      const uniqueUrls = new Set<string>();
+
+      for (const result of results) {
+        if (!result.content || uniqueUrls.has(result.url)) continue;
+
+        const stripHtmlTags = (str: string) => str.replace(/<[^>]*>?/gm, "");
+
+        const content = stripHtmlTags(result.content).trim();
+
+        if (content === "") continue;
+
+        const title = stripHtmlTags(result.title);
+
+        const url = result.url;
+
+        searchResults.push([title, content, url]);
+
+        uniqueUrls.add(url);
+      }
+    }
+
+    return searchResults;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
