73d15b257a820bae352d75bd11900ec0a46c2cd4
Victor Nogueira <felladrin@gmail.com>
2024-04-24 01:45:15 +0300

    Reranking search results with transformers.js or wllama is too heavy for mobile browsers, so use only TensorFlow when running on mobile

--- client/modules/textGeneration.ts
+++ client/modules/textGeneration.ts
@@ -66,20 +66,20 @@ export async function prepareTextGeneration() {
   updateLoadingToast("Loading AI model...");
 
   try {
-    updateSearchResults(
-      await (
-        "SharedArrayBuffer" in window
-          ? rankSearchResultsWithWllama
-          : rankSearchResultsWithTransformers
-      )(searchResults, query),
-    );
+    if (isRunningOnMobile) {
+      throw Error("Mobile device detected. Skipping to TensorFlow.");
+    }
+
+    try {
+      updateSearchResults(
+        await rankSearchResultsWithWllama(searchResults, query),
+      );
+    } catch (error) {
+      updateSearchResults(
+        await rankSearchResultsWithTransformers(searchResults, query),
+      );
+    }
   } catch (error) {
-    console.info(dedent`
-      Could not rerank search results using transformers.js or wllama.
-
-      Falling back to TensorFlow.js.
-    `);
-
     updateSearchResults(
       await rankSearchResultsWithTensorFlow(searchResults, query),
     );
