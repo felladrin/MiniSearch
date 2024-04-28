47ed1b13c9c6519b48de855535577ca9f5bb3517
Victor Nogueira <felladrin@gmail.com>
2024-04-28 15:32:49 +0300

    Reformat file

--- client/modules/pubSub.ts
+++ client/modules/pubSub.ts
@@ -20,26 +20,36 @@ export const disableAiResponseSettingPubSub = createLocalStoragePubSub(
   "disableAiResponse",
   false,
 );
+
 export const [, , getDisableAiResponseSetting] = disableAiResponseSettingPubSub;
 
 export const summarizeLinksSettingPubSub = createLocalStoragePubSub(
   "summarizeLinks",
   false,
 );
+
 export const [, , getSummarizeLinksSetting] = summarizeLinksSettingPubSub;
 
 export const useLargerModelSettingPubSub = createLocalStoragePubSub(
   "useLargerModel",
   false,
 );
+
 export const [, , getUseLargerModelSetting] = useLargerModelSettingPubSub;
 
 export const promptPubSub = createPubSub("");
+
 export const [updatePrompt] = promptPubSub;
+
 export const responsePubSub = createPubSub("");
+
 export const [updateResponse] = responsePubSub;
+
 export const searchResultsPubSub = createPubSub<SearchResults>([]);
+
 export const [updateSearchResults, , getSearchResults] = searchResultsPubSub;
+
 export const urlsDescriptionsPubSub = createPubSub<Record<string, string>>({});
+
 export const [updateUrlsDescriptions, , getUrlsDescriptions] =
   urlsDescriptionsPubSub;
