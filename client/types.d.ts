953d5ec1b6b87825e8c430c3d202b1e9eac7fa6d
Victor Nogueira <felladrin@gmail.com>
2024-05-01 04:46:42 +0300

    Defines query suggestions at build time, to avoid having them defined in client code

--- client/types.d.ts
+++ client/types.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
 
 declare const __SEARCH_TOKEN__: string;
+declare const __QUERY_SUGGESTIONS__: string[];
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 import { writeFileSync, readFileSync, existsSync } from "node:fs";
 import temporaryDirectory from "temp-dir";
 import path from "node:path";
+import fs from "node:fs";
 
 const serverStartTime = new Date().getTime();
 let searchesSinceLastRestart = 0;
@@ -17,6 +18,7 @@ export default defineConfig(({ command }) => {
     root: "./client",
     define: {
       __SEARCH_TOKEN__: JSON.stringify(getSearchToken()),
+      __QUERY_SUGGESTIONS__: getQuerySuggestions(25),
     },
     server: {
       host: process.env.HOST,
@@ -247,4 +249,12 @@ function regenerateSearchToken() {
 function getSearchToken() {
   if (!existsSync(getSearchTokenFilePath())) regenerateSearchToken();
   return readFileSync(getSearchTokenFilePath(), "utf8");
+}
+
+function getQuerySuggestions(limit?: number) {
+  return (
+    JSON.parse(fs.readFileSync("query-suggestions.json").toString()) as string[]
+  )
+    .sort(() => Math.random() - 0.5)
+    .slice(0, limit);
 }
