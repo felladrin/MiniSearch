a08a047d71f005172b1b3291bdf191e0c05d18cd
Victor Nogueira <felladrin@gmail.com>
2024-05-09 20:36:55 +0300

    Close the Settings Toast if it's open when the Settings button is clicked

--- client/components/SettingsButton.tsx
+++ client/components/SettingsButton.tsx
@@ -1,43 +1,54 @@
+import { useState } from "react";
 import { SettingsForm } from "./SettingsForm";
 import { toast } from "react-hot-toast";
 
 export function SettingsButton() {
+  const [isToastOpen, setToastOpen] = useState(false);
+
   const toastId = "settings-toast";
+
+  const openToast = () => {
+    setToastOpen(true);
+
+    toast(
+      <div>
+        <SettingsForm />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "8px",
+          }}
+        >
+          <button style={{ fontSize: "small" }} onClick={closeToast}>
+            Done
+          </button>
+        </div>
+      </div>,
+      {
+        id: toastId,
+        duration: Infinity,
+        position: "bottom-center",
+        style: {
+          borderRadius: "10px",
+          background: "var(--background)",
+          color: "var(--text-main)",
+        },
+      },
+    );
+  };
+
+  const closeToast = () => {
+    setToastOpen(false);
+    toast.dismiss(toastId);
+  };
 
   return (
     <button
       style={{ fontSize: "small", marginRight: 0 }}
       onClick={(event) => {
         event.preventDefault();
-        toast(
-          <div>
-            <SettingsForm />
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "8px",
-              }}
-            >
-              <button
-                style={{ fontSize: "small" }}
-                onClick={() => toast.dismiss(toastId)}
-              >
-                Done
-              </button>
-            </div>
-          </div>,
-          {
-            id: toastId,
-            duration: Infinity,
-            position: "bottom-center",
-            style: {
-              borderRadius: "10px",
-              background: "var(--background)",
-              color: "var(--text-main)",
-            },
-          },
-        );
+        isToastOpen ? closeToast() : openToast();
       }}
     >
       Settings
