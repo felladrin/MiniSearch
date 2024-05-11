c9f8f733c7e9a2e2adbd1b11cc1b9176e4317003
Victor Nogueira <felladrin@gmail.com>
2024-05-11 03:25:22 +0300

    Update the larger model used on mobiles when running with Wllama

--- client/modules/textGeneration.ts
+++ client/modules/textGeneration.ts
@@ -258,18 +258,10 @@ async function generateTextWithWllama() {
       sampling: commonSamplingConfig,
     },
     mobileLarger: {
-      url: Array.from(
-        { length: 3 },
-        (_, i) =>
-          `https://huggingface.co/Felladrin/gguf-sharded-Qwen1.5-0.5B-Chat/resolve/main/Qwen1.5-0.5B-Chat.Q4_k_m.shard-${(
-            i + 1
-          )
-            .toString()
-            .padStart(5, "0")}-of-00003.gguf`,
-      ),
-      userPrefix: "<|im_start|>user\n",
-      assistantPrefix: "<|im_start|>assistant\n",
-      messageSuffix: "<|im_end|>\n",
+      url: "https://huggingface.co/Felladrin/gguf-zephyr-220m-dpo-full/resolve/main/zephyr-220m-dpo-full.Q8_0.gguf",
+      userPrefix: "<|user|>\n",
+      assistantPrefix: "<|assistant|>\n",
+      messageSuffix: "</s>\n",
       sampling: commonSamplingConfig,
     },
     desktopDefault: {
