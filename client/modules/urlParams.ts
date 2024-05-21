cbef66c277f327e616694a58f3f77d68919bb987
Victor Nogueira <felladrin@gmail.com>
2024-05-21 10:01:07 +0300

    Remove unused `beta` parameter

--- client/modules/urlParams.ts
+++ client/modules/urlParams.ts
@@ -1,4 +1,3 @@
 const urlParams = new URLSearchParams(self.location.search);
 export const debug = urlParams.has("debug");
-export const beta = urlParams.has("beta");
 export const query = urlParams.get("q");
