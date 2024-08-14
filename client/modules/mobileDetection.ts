eac051f5c6c2f68245a9deaf12aec05725745587
Victor Nogueira <felladrin@gmail.com>
2024-08-14 10:01:32 +0200

    Consider running in mobile when the device is a phone or a tablet

--- client/modules/mobileDetection.ts
+++ client/modules/mobileDetection.ts
@@ -1,4 +1,6 @@
 import MobileDetect from "mobile-detect";
 
-export const isRunningOnMobile =
-  new MobileDetect(self.navigator.userAgent).mobile() !== null;
+export const isRunningOnMobile = (() => {
+  const { phone, tablet } = new MobileDetect(self.navigator.userAgent);
+  return phone() !== null || tablet() !== null;
+})();
