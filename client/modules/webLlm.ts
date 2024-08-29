801cd3d55435ba1d741aaa0946029c407db7f430
Victor Nogueira <felladrin@gmail.com>
2024-08-30 00:19:19 +0200

    Add Gemma 2 2B to the list of available WebGPU models

--- client/modules/webLlm.ts
+++ client/modules/webLlm.ts
@@ -1,6 +1,6 @@
 export const webLlmModels = [
   {
-    label: "Small (Qwen2 0.5B Instruct)",
+    label: "Tiny (Qwen2 0.5B Instruct)",
     model_id: "mlc-q4f16_1-Qwen2-0.5B-Instruct",
     model: "https://huggingface.co/Felladrin/mlc-q4f16_1-Qwen2-0.5B-Instruct",
     model_lib:
@@ -12,15 +12,27 @@ export const webLlmModels = [
     },
   },
   {
-    label: "Medium (Arcee Lite)",
-    model_id: "mlc-q0f16-arcee-lite",
-    model: "https://huggingface.co/Felladrin/mlc-q0f16-arcee-lite",
+    label: "Small (Arcee Lite)",
+    model_id: "mlc-q4f16_1-arcee-lite",
+    model: "https://huggingface.co/Felladrin/mlc-q4f16_1-arcee-lite",
     model_lib:
-      "https://huggingface.co/Felladrin/mlc-q0f16-arcee-lite/resolve/main/model.wasm",
+      "https://huggingface.co/Felladrin/mlc-q4f16_1-arcee-lite/resolve/main/model.wasm",
     overrides: {
       context_window_size: 2048,
       temperature: 0,
       frequency_penalty: 1.02,
+    },
+  },
+  {
+    label: "Medium (Gemma 2 2B Instruct)",
+    model_id: "mlc-q4f16_1-gemma-2-2b-it",
+    model: "https://huggingface.co/Felladrin/mlc-q4f16_1-gemma-2-2b-it",
+    model_lib:
+      "https://huggingface.co/Felladrin/mlc-q4f16_1-gemma-2-2b-it/resolve/main/model.wasm",
+    overrides: {
+      context_window_size: 2048,
+      temperature: 0.7,
+      top_p: 0.7,
     },
   },
   {
