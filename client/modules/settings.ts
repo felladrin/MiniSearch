e368bad8476914883379b31e7f6b4d5f39c1a0c1
Victor Nogueira <felladrin@gmail.com>
2024-09-06 00:10:14 +0200

    Add more models to run on CPU

--- client/modules/settings.ts
+++ client/modules/settings.ts
@@ -17,7 +17,7 @@ export const defaultSettings = {
   [Setting.webLlmModelId]: isF16Supported
     ? "Qwen2-0.5B-Instruct-q4f16_1-MLC"
     : "TinyLlama-1.1B-Chat-v1.0-q4f32_1-MLC",
-  [Setting.wllamaModelId]: "smollm-360M-instruct",
+  [Setting.wllamaModelId]: "default",
   [Setting.cpuThreads]: 1,
   [Setting.searchResultsToConsider]: 3,
   [Setting.backgroundImageUrl]: defaultBackgroundImageUrl,
--- client/modules/wllama.ts
+++ client/modules/wllama.ts
@@ -40,19 +40,6 @@ export async function initializeWllama(
   return wllama;
 }
 
-const commonSamplingConfig: SamplingConfig = {
-  temp: 0.2,
-  dynatemp_range: 0.15,
-  top_k: 0,
-  top_p: 1,
-  min_p: 0.1,
-  typical_p: 0.85,
-  penalty_repeat: 1.176,
-  penalty_last_n: -1,
-  mirostat: 2,
-  mirostat_tau: 3.5,
-};
-
 export interface WllamaModel {
   label: string;
   url: string | string[];
@@ -67,16 +54,15 @@ export interface WllamaModel {
   ) => Promise<string>;
 }
 
-export const wllamaModels: Record<string, WllamaModel> = {
-  "smollm-360M-instruct": {
-    label: "SmolLM 360M Instruct • 290 MB",
-    url: "https://huggingface.co/Felladrin/gguf-Q5_K_M-smollm-360M-instruct-add-basics/resolve/main/smollm-360m-instruct-add-basics-q5_k_m-imat-00001-of-00005.gguf",
-    buildPrompt: (wllama, query, searchResults) =>
-      formatChat(wllama, [
-        {
-          id: 0,
-          role: "system",
-          content: `You are an AI assistant tasked with answering questions based on provided web search results and a user inquiry. Analyze the search results and use them as background information if relevant to the inquiry, but you may disregard them if they don't contribute to answering the question.
+const defaultModelConfig: WllamaModel = {
+  label: "SmolLM 135M • 145 MB",
+  url: "https://huggingface.co/Felladrin/gguf-Q8_0-smollm-135M-instruct-v0.2/resolve/main/smollm-135m-instruct-add-basics-q8_0.gguf",
+  buildPrompt: (wllama, query, searchResults) =>
+    formatChat(wllama, [
+      {
+        id: 0,
+        role: "system",
+        content: `You are an AI assistant tasked with answering questions based on provided web search results and a user inquiry. Analyze the search results and use them as background information if relevant to the inquiry, but you may disregard them if they don't contribute to answering the question.
 
 Web search results:
 ${"```"}
@@ -84,37 +70,88 @@ ${searchResults}
 ${"```"}
 
 Please answer the user's inquiry based on the information provided or your general knowledge if the search results are not relevant. Include additional context or related information that might be useful or interesting to the user, even if not directly asked for in the inquiry. Always respond in the same language used by the user in their inquiry.`,
-        },
-        { id: 1, role: "user", content: query },
-      ]),
-    cacheType: "f16",
-    contextSize: 1280,
-    shouldIncludeUrlsOnPrompt: false,
-    sampling: commonSamplingConfig,
+      },
+      { id: 1, role: "user", content: query },
+    ]),
+  cacheType: "f16",
+  contextSize: 2048,
+  shouldIncludeUrlsOnPrompt: false,
+  sampling: {
+    temp: 0.2,
+    dynatemp_range: 0.15,
+    top_k: 0,
+    top_p: 1,
+    min_p: 0.1,
+    typical_p: 0.85,
+    penalty_repeat: 1.176,
+    penalty_last_n: -1,
+    mirostat: 2,
+    mirostat_tau: 3.5,
+  },
+};
+
+export const wllamaModels: Record<string, WllamaModel> = {
+  default: defaultModelConfig,
+  "smollm-360m": {
+    ...defaultModelConfig,
+    label: "SmolLM 360M • 290 MB",
+    url: "https://huggingface.co/Felladrin/gguf-Q5_K_M-smollm-360M-instruct-add-basics/resolve/main/smollm-360m-instruct-add-basics-q5_k_m-imat-00001-of-00005.gguf",
+  },
+  "danube-3-500m": {
+    ...defaultModelConfig,
+    label: "Danube 3 500M • 368 MB",
+    url: "https://huggingface.co/Felladrin/gguf-q5_k_m-h2o-danube3-500m-chat/resolve/main/h2o-danube3-500m-chat-q5_k_m-imat-00001-of-00003.gguf",
+    buildPrompt: (_, query, searchResults) =>
+      Promise.resolve(`You are an AI assistant tasked with answering questions based on provided web search results and a user inquiry. Analyze the search results and use them as background information if relevant to the inquiry, but you may disregard them if they don't contribute to answering the question.
+
+Web search results:
+${"```"}
+${searchResults}  
+${"```"}
+
+Please answer the user's inquiry based on the information provided or your general knowledge if the search results are not relevant. Include additional context or related information that might be useful or interesting to the user, even if not directly asked for in the inquiry. Always respond in the same language used by the user in their inquiry.
+<|prompt|>
+${query}</s>
+<|answer|>
+`),
+  },
+  "qwen-2-0.5b": {
+    ...defaultModelConfig,
+    label: "Qwen 2 0.5B • 420 MB",
+    url: "https://huggingface.co/Felladrin/gguf-q5_k_m-imat-qwen2-0.5b-instruct/resolve/main/qwen2-0-00001-of-00003.gguf",
+  },
+  "tinydolphin-1.1b": {
+    ...defaultModelConfig,
+    label: "TinyDolphin 1.1B • 782 MB",
+    url: "https://huggingface.co/Felladrin/gguf-q5_k_m-tinydolphin-2.8.2-1.1b-laser/resolve/main/tinydolphin-2-00001-of-00009.gguf",
   },
   "arcee-lite": {
-    label: "Arcee Lite • 1.43 GB",
+    ...defaultModelConfig,
+    label: "Arcee Lite 1.5B • 1.43 GB",
     url: "https://huggingface.co/Felladrin/gguf-q5_k_l-imat-arcee-lite/resolve/main/arcee-lite-Q5_K_L.shard-00001-of-00006.gguf",
-    buildPrompt: (wllama, query, searchResults) =>
-      formatChat(wllama, [
-        {
-          id: 0,
-          role: "system",
-          content: `You are an AI assistant tasked with answering questions based on provided web search results and a user inquiry. Analyze the search results and use them as background information if relevant to the inquiry, but you may disregard them if they don't contribute to answering the question.
+  },
+  "gemma-2-2b": {
+    ...defaultModelConfig,
+    label: "Gemma 2 2B • 1.92 GB",
+    url: "https://huggingface.co/Felladrin/gguf-sharded-gemma-2-2b-it-abliterated/resolve/main/gemma-2-2b-it-abliterated-q5_k_m-imat-00001-of-00009.gguf",
+    buildPrompt: (_, query, searchResults) =>
+      Promise.resolve(`You are an AI assistant tasked with answering questions based on provided web search results and a user inquiry. Analyze the search results and use them as background information if relevant to the inquiry, but you may disregard them if they don't contribute to answering the question.
 
-Web search results:  
+Web search results:
 ${"```"}
-${searchResults}
+${searchResults}  
 ${"```"}
 
-Please answer the user's inquiry based on the information provided or your general knowledge if the search results are not relevant. Include additional context or related information that might be useful or interesting to the user, even if not directly asked for in the inquiry. Always respond in the same language used by the user in their inquiry.`,
-        },
-        { id: 1, role: "user", content: query },
-      ]),
-    cacheType: "f16",
-    contextSize: 1280,
-    shouldIncludeUrlsOnPrompt: false,
-    sampling: commonSamplingConfig,
+Please answer the user's inquiry based on the information provided or your general knowledge if the search results are not relevant. Include additional context or related information that might be useful or interesting to the user, even if not directly asked for in the inquiry. Always respond in the same language used by the user in their inquiry.
+<bos><start_of_turn>user
+${query}<end_of_turn>
+<start_of_turn>model
+`),
+  },
+  "phi-3.5-mini-3.8b": {
+    ...defaultModelConfig,
+    label: "Phi 3.5 Mini 3.8B • 2.82 GB",
+    url: "https://huggingface.co/Felladrin/gguf-q5_k_m-phi-3.5-mini-instruct/resolve/main/phi-3-00001-of-00025.gguf",
   },
 };
 
