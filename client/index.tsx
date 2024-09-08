3d0bf249192f49308133efbc0a4fc9617dd1e41d
Victor Nogueira <felladrin@gmail.com>
2024-09-08 17:59:44 +0200

    Enable StrictMode in React dev mode

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App/App";
 import "./index.less";
+import { StrictMode } from "react";
 
 createRoot(document.body.appendChild(document.createElement("div"))).render(
-  <App />,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
