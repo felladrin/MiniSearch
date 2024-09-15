e0e890770069c820773426b190fcf85f566e63ea
Victor Nogueira <felladrin@gmail.com>
2024-09-15 21:12:07 +0200

    Bring back transition effect to the image results list

--- client/components/App/Main/ImageResultsList/ImageResultsList.tsx
+++ client/components/App/Main/ImageResultsList/ImageResultsList.tsx
@@ -1,23 +1,40 @@
 import { Carousel } from "@mantine/carousel";
 import { SearchResults } from "../../../../modules/search";
 import { ImageResult } from "./ImageResult";
+import { useState, useEffect } from "react";
+import { Transition } from "@mantine/core";
 
 export function ImageResultsList({
   imageResults,
 }: {
   imageResults: SearchResults["imageResults"];
 }) {
+  const [isMounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
   return (
     <Carousel slideSize="0" slideGap="xs" align="start" dragFree loop>
-      {imageResults.map(([title, url, thumbnailUrl, sourceUrl]) => (
-        <Carousel.Slide key={sourceUrl}>
-          <ImageResult
-            title={title}
-            url={url}
-            thumbnailUrl={thumbnailUrl}
-            sourceUrl={sourceUrl}
-          />
-        </Carousel.Slide>
+      {imageResults.map(([title, url, thumbnailUrl, sourceUrl], index) => (
+        <Transition
+          key={sourceUrl}
+          mounted={isMounted}
+          transition="fade"
+          timingFunction="ease"
+          enterDelay={index * 250}
+          duration={1500}
+        >
+          {(styles) => (
+            <Carousel.Slide style={styles}>
+              <ImageResult
+                title={title}
+                url={url}
+                thumbnailUrl={thumbnailUrl}
+                sourceUrl={sourceUrl}
+              />
+            </Carousel.Slide>
+          )}
+        </Transition>
       ))}
     </Carousel>
   );
