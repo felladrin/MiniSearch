06a83c04fae5aa38a2eea342a6064eeb6a2c63fb
Victor Nogueira <felladrin@gmail.com>
2024-09-16 18:30:34 +0200

    Extract `CustomZoomContent` component from `ImageResult`

--- client/components/App/Main/ImageResultsList/ImageResult.tsx
+++ client/components/App/Main/ImageResultsList/ImageResult.tsx
@@ -1,8 +1,6 @@
-import { Stack, Button, Text, Notification, Group, rem } from "@mantine/core";
+import { rem } from "@mantine/core";
 import Zoom from "react-medium-image-zoom";
-import { getHostname } from "../../../../modules/stringFormatters";
-import { addLogEntry } from "../../../../modules/logEntries";
-import { useEffect, useState } from "react";
+import { CustomZoomContent } from "./CustomZoomContent";
 
 export function ImageResult({
   title,
@@ -15,72 +13,20 @@ export function ImageResult({
   thumbnailUrl: string;
   sourceUrl: string;
 }) {
-  const CustomZoomContent: React.ComponentProps<typeof Zoom>["ZoomContent"] = ({
-    buttonUnzoom,
-    img,
-    modalState,
-  }) => {
-    const [showNotification, setShowNotification] = useState(true);
-
-    useEffect(() => setShowNotification(true), [modalState]);
-
-    return (
-      <>
-        {img}
-        {modalState === "LOADED" && (
-          <>
-            {buttonUnzoom}
-            <Stack justify="center" m={16} pos="absolute" inset="auto 0 0 0">
-              {showNotification && (
-                <Notification
-                  onClose={() => setShowNotification(false)}
-                  radius="xl"
-                  color="transparent"
-                  withBorder
-                >
-                  <Stack align="center" gap="md">
-                    <Text component="cite" ta="center">
-                      {title || "Untitled"}
-                    </Text>
-                    <Group align="center" justify="center" gap="xs">
-                      <Button
-                        variant="subtle"
-                        component="a"
-                        size="xs"
-                        href={sourceUrl}
-                        target="_blank"
-                        title="Click to see the image in full size"
-                        rel="noopener noreferrer"
-                      >
-                        View in full resolution
-                      </Button>
-                      <Button
-                        variant="subtle"
-                        component="a"
-                        href={url}
-                        target="_blank"
-                        size="xs"
-                        title="Click to visit the page where the image was found"
-                        rel="noopener noreferrer"
-                        onClick={() => {
-                          addLogEntry("User clicked an image result");
-                        }}
-                      >
-                        Visit {getHostname(url)}
-                      </Button>
-                    </Group>
-                  </Stack>
-                </Notification>
-              )}
-            </Stack>
-          </>
-        )}
-      </>
-    );
-  };
-
   return (
-    <Zoom ZoomContent={CustomZoomContent} classDialog="custom-zoom">
+    <Zoom
+      ZoomContent={({ buttonUnzoom, img, modalState }) => (
+        <CustomZoomContent
+          closeButtonComponent={buttonUnzoom}
+          imageComponent={img}
+          opened={modalState === "LOADED"}
+          title={title}
+          sourceUrl={sourceUrl}
+          url={url}
+        />
+      )}
+      classDialog="custom-zoom"
+    >
       <img
         alt={title}
         src={thumbnailUrl}
