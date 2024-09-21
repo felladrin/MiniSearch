2fcd3d74cb9d34cf96da54a71e1a7f62b8b642c8
Victor Nogueira <felladrin@gmail.com>
2024-09-21 22:13:46 +0200

    Optimize `sleep` module

--- client/modules/sleep.ts
+++ client/modules/sleep.ts
@@ -1,11 +1,9 @@
-export async function sleep(milliseconds: number) {
+async function sleep(milliseconds: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
   });
 }
 
-export async function sleepUntilIdle() {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 0);
-  });
+export function sleepUntilIdle() {
+  return sleep(0);
 }
