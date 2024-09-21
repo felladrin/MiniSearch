b0a8792047b64fb59d4776c37ea1aee5ff70b8f4
Victor Nogueira <felladrin@gmail.com>
2024-09-21 20:50:25 +0200

    Add dark mode toggle functionality to `InterfaceSettingsForm` component

--- client/components/Menu/InterfaceSettingsForm.tsx
+++ client/components/Menu/InterfaceSettingsForm.tsx
@@ -1,6 +1,11 @@
 import { usePubSub } from "create-pubsub/react";
 import { settingsPubSub } from "../../modules/pubSub";
-import { Stack, Switch } from "@mantine/core";
+import {
+  Stack,
+  Switch,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 export function InterfaceSettingsForm() {
@@ -9,9 +14,22 @@ export function InterfaceSettingsForm() {
     initialValues: settings,
     onValuesChange: setSettings,
   });
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light");
+
+  const toggleColorScheme = () => {
+    setColorScheme(computedColorScheme === "dark" ? "light" : "dark");
+  };
 
   return (
     <Stack gap="md">
+      <Switch
+        label="Dark Mode"
+        checked={computedColorScheme === "dark"}
+        onChange={toggleColorScheme}
+        description="Enable or disable the dark color scheme."
+      />
+
       <Switch
         label="Image Search"
         {...form.getInputProps("enableImageSearch", {
