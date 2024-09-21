7d2bcd953a6a77f38b1e803258199e0e9939c8de
Victor Nogueira <felladrin@gmail.com>
2024-09-21 19:12:18 +0200

    Hide also "Instructions for AI" setting if "AI Response" is disabled

--- client/components/Menu/AISettingsForm.tsx
+++ client/components/Menu/AISettingsForm.tsx
@@ -224,69 +224,69 @@ export function AISettingsForm() {
                   .otherwise(() => null)}
               </>
             )}
+
+            <Textarea
+              label="Instructions for AI"
+              descriptionProps={{ component: "div" }}
+              description={
+                <>
+                  <span>
+                    Customize instructions for the AI to tailor its responses.
+                  </span>
+                  <br />
+                  <span>For example:</span>
+                  <ul>
+                    <li>
+                      Specify preferences
+                      <ul>
+                        <li>
+                          <em>"use simple language"</em>
+                        </li>
+                        <li>
+                          <em>"provide step-by-step explanations"</em>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      Set a response style
+                      <ul>
+                        <li>
+                          <em>"answer in a friendly tone"</em>
+                        </li>
+                        <li>
+                          <em>"write your response in Spanish"</em>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      Provide context about the audience
+                      <ul>
+                        <li>
+                          <em>"you're talking to a high school student"</em>
+                        </li>
+                        <li>
+                          <em>
+                            "consider that your audience is composed of
+                            professionals in the field of graphic design"
+                          </em>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                  <span>
+                    Note: The special tag <em>{`{{searchResults}}`}</em> will be
+                    replaced with the search results, while{" "}
+                    <em>{`{{dateTime}}`}</em> will be replaced with the current
+                    date and time.
+                  </span>
+                </>
+              }
+              autosize
+              maxRows={10}
+              {...form.getInputProps("systemPrompt")}
+            />
           </>
         )}
-
-        <Textarea
-          label="Instructions for AI"
-          descriptionProps={{ component: "div" }}
-          description={
-            <>
-              <span>
-                Customize instructions for the AI to tailor its responses.
-              </span>
-              <br />
-              <span>For example:</span>
-              <ul>
-                <li>
-                  Specify preferences
-                  <ul>
-                    <li>
-                      <em>"use simple language"</em>
-                    </li>
-                    <li>
-                      <em>"provide step-by-step explanations"</em>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  Set a response style
-                  <ul>
-                    <li>
-                      <em>"answer in a friendly tone"</em>
-                    </li>
-                    <li>
-                      <em>"write your response in Spanish"</em>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  Provide context about the audience
-                  <ul>
-                    <li>
-                      <em>"you're talking to a high school student"</em>
-                    </li>
-                    <li>
-                      <em>
-                        "consider that your audience is composed of
-                        professionals in the field of graphic design"
-                      </em>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <span>
-                Note: The special tag <em>{`{{searchResults}}`}</em> will be
-                replaced with the search results, while{" "}
-                <em>{`{{dateTime}}`}</em> will be replaced with the current date
-                and time.
-              </span>
-            </>
-          }
-          autosize
-          maxRows={10}
-          {...form.getInputProps("systemPrompt")}
-        />
       </Stack>
     </form>
   );
