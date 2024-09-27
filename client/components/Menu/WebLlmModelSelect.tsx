cdd5f8c5f438690403a10e70e1c9d916be5278ef
Victor Nogueira <felladrin@gmail.com>
2024-09-27 10:22:55 +0200

    Directly import `prebuiltAppConfig`, as `WebLlmModelSelect` component is now lazily loaded, so it displays a skeleton while the WebLLM module is loaded

--- client/components/Menu/WebLlmModelSelect.tsx
+++ client/components/Menu/WebLlmModelSelect.tsx
@@ -1,6 +1,7 @@
-import { ComboboxData, Select } from "@mantine/core";
+import { ComboboxItem, Select } from "@mantine/core";
 import { useState, useEffect } from "react";
 import { isF16Supported } from "../../modules/webGpu";
+import { prebuiltAppConfig } from "@mlc-ai/web-llm";
 
 export default function WebLlmModelSelect({
   value,
@@ -9,33 +10,29 @@ export default function WebLlmModelSelect({
   value: string;
   onChange: (value: string) => void;
 }) {
-  const [webGpuModels, setWebGpuModels] = useState<ComboboxData>([]);
+  const [webGpuModels] = useState<ComboboxItem[]>(() => {
+    const suffix = isF16Supported ? "-q4f16_1-MLC" : "-q4f32_1-MLC";
+
+    const models = prebuiltAppConfig.model_list
+      .filter((model) => model.model_id.endsWith(suffix))
+      .sort((a, b) => (a.vram_required_MB ?? 0) - (b.vram_required_MB ?? 0))
+      .map((model) => ({
+        label: `${model.model_id.replace(suffix, "")} • ${Math.round(model.vram_required_MB ?? 0) || "N/A"} MB`,
+        value: model.model_id,
+      }));
+
+    return models;
+  });
 
   useEffect(() => {
-    async function loadWebGpuModels() {
-      const { prebuiltAppConfig } = await import("@mlc-ai/web-llm");
+    const isCurrentModelValid = webGpuModels.some(
+      (model) => model.value === value,
+    );
 
-      const suffix = isF16Supported ? "-q4f16_1-MLC" : "-q4f32_1-MLC";
-
-      const models = prebuiltAppConfig.model_list
-        .filter((model) => model.model_id.endsWith(suffix))
-        .sort((a, b) => (a.vram_required_MB ?? 0) - (b.vram_required_MB ?? 0))
-        .map((model) => ({
-          label: `${model.model_id.replace(suffix, "")} • ${Math.round(model.vram_required_MB ?? 0) || "N/A"} MB`,
-          value: model.model_id,
-        }));
-
-      setWebGpuModels(models);
-
-      const isCurrentModelValid = models.some((model) => model.value === value);
-
-      if (!isCurrentModelValid && models.length > 0) {
-        onChange(models[0].value);
-      }
+    if (!isCurrentModelValid && webGpuModels.length > 0) {
+      onChange(webGpuModels[0].value);
     }
-
-    loadWebGpuModels();
-  }, [value, onChange]);
+  }, []);
 
   const handleChange = (value: string | null) => {
     if (value) onChange(value);
