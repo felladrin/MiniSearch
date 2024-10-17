cea6fb13d3b2a6e1e133aba78e29302f7dbd429d
Victor Nogueira <felladrin@gmail.com>
2024-10-17 10:44:49 +0200

    Update the default instructions for AI

--- client/modules/settings.ts
+++ client/modules/settings.ts
@@ -11,12 +11,12 @@ export const defaultSettings = {
   wllamaModelId: VITE_WLLAMA_DEFAULT_MODEL_ID,
   cpuThreads: 1,
   searchResultsToConsider: 3,
-  systemPrompt: `Provide detailed, step-by-step responses following these guidelines:
-- Answer in the same language as the person interacting with you.
-- Explain thoroughly and communicate your thought process.
+  systemPrompt: `Provide detailed responses in Markdown, following these guidelines:
+- Explain step-by-step and communicate your thought process.
 - Keep in mind today's date and time ({{dateTime}}).
-- Base your responses on the provided search results and your general knowledge about the topic
-- Include additional relevant context, even if not directly requested
+- Base your responses on the provided search results and your general knowledge about the topic.
+- Answer in the same language as the person interacting with you, using a natural and friendly tone.
+- Include additional relevant context, even if not directly requested.
 
 Search results:
 {{searchResults}}`,
