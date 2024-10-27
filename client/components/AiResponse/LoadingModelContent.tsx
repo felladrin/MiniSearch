d6c79f56141b4059f31599c61ee12bfb82691f3b
Victor Nogueira <felladrin@gmail.com>
2024-10-27 16:03:40 +0100

    Don't show the numbers below the progress bar until they are ready to be shown

--- client/components/AiResponse/LoadingModelContent.tsx
+++ client/components/AiResponse/LoadingModelContent.tsx
@@ -7,10 +7,11 @@ export default function LoadingModelContent({
   modelLoadingProgress: number;
   modelSizeInMegabytes: number;
 }) {
-  const isLoadingComplete =
-    modelLoadingProgress === 100 || modelLoadingProgress === 0;
-  const percent = isLoadingComplete ? 100 : modelLoadingProgress;
-  const strokeColor = isLoadingComplete ? "#52c41a" : "#3385ff";
+  const isLoadingStarting = modelLoadingProgress === 0;
+  const isLoadingComplete = modelLoadingProgress === 100;
+  const percent =
+    isLoadingComplete || isLoadingStarting ? 100 : modelLoadingProgress;
+  const strokeColor = percent === 100 ? "#52c41a" : "#3385ff";
   const downloadedSize = (modelSizeInMegabytes * modelLoadingProgress) / 100;
   const sizeText = `${downloadedSize.toFixed(0)} MB / ${modelSizeInMegabytes.toFixed(0)} MB`;
 
@@ -22,14 +23,16 @@ export default function LoadingModelContent({
       <Card.Section withBorder inheritPadding py="md">
         <Stack gap="xs">
           <Progress color={strokeColor} value={percent} animated />
-          <Group justify="space-between">
-            <Text size="sm" c="dimmed">
-              {sizeText}
-            </Text>
-            <Text size="sm" c="dimmed">
-              {percent.toFixed(1)}%
-            </Text>
-          </Group>
+          {!isLoadingStarting && (
+            <Group justify="space-between">
+              <Text size="sm" c="dimmed">
+                {sizeText}
+              </Text>
+              <Text size="sm" c="dimmed">
+                {percent.toFixed(1)}%
+              </Text>
+            </Group>
+          )}
         </Stack>
       </Card.Section>
     </Card>
