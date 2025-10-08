43fd75a0d49d833fc6dc8b0b50a898e20e97508f
Victor Nogueira <felladrin@gmail.com>
2025-10-08 14:34:18 +0200

    Format `dateTime` using `Intl.DateTimeFormat` instead of `Date.toString()`

--- client/modules/systemPrompt.ts
+++ client/modules/systemPrompt.ts
@@ -3,5 +3,12 @@ import { getSettings } from "./pubSub";
 export function getSystemPrompt(searchResults: string) {
   return getSettings()
     .systemPrompt.replace("{{searchResults}}", searchResults)
-    .replace("{{dateTime}}", new Date().toString());
+    .replace(
+      "{{dateTime}}",
+      new Intl.DateTimeFormat("en-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }).format(new Date()),
+    );
 }
